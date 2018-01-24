78017b9681560b634d136ce6607f33d2e5b57923
xiefei16 <xiefei16@foxmail.com>
2018-01-24 19:17:59 +0800

    添加文件

--- Python/analysis/huabei.py
+++ Python/analysis/huabei.py
@@ -2,7 +2,14 @@
 import pandas as pd
 
 df = pd.read_csv('/Users/xiefei/Desktop/reject_reason', sep= '\t')
+df['hit_name'] = df['hit_name'].apply(lambda x : x[len(x-3),len(x)-1])
 
-df.groupby()
+a = df.groupby(['hit_name'])['user_id'].nunique().reset_index()
+print a
+
+def getZmscore(x):
+    para = str(x)
+    if len(para) > 3:
+        score = para[]
 
 
